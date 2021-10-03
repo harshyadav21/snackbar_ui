328b8341a64ad0ec34fb08b707da65f29ba6644a
Shivam Bist <63891132+Shivambisht0@users.noreply.github.com>
2021-10-03 16:14:56 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
       home: Scaffold(appBar: AppBar(title: Text('hello'),
       actions: <Widget>[Icon(Icons.accessibility)

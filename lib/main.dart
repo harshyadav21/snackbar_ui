8111917b8e2153594821ebf45c5bfef0c25c3e41
harshraj_2121 <yadavharsh7945@gmail.com>
2021-10-03 15:29:14 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: Scaffold(appBar: AppBar(title: Text('hello'),
+      actions: <Widget>[Icon(Icons.accessibility)
+      ],
+      
+      ),
+      
+      body: MyAppBody(),
+      ),
+      
+    );
+  }
+}
+
+
+
+class MyAppBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        // ignore: deprecated_member_use
+        child: RaisedButton(onPressed: () { print('hii');
+        },
+         child: Text('clcik'),),
+        
+      ),
+    );
+  }
+}
